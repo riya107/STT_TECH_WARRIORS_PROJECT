a5b9414126222d30b224b30c0dc3c5a244d597c8
Ankita Kumari <akankitakumari009@gmail.com>
2022-03-05 23:24:35 +0530

    Functionality added in Homepage

--- front_end/src/components/Home.js
+++ front_end/src/components/Home.js
@@ -1,14 +1,31 @@
-function Home() {
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
 
+
+let exportuser;
+
+function Home() {
+    const navigate=useNavigate()
+    const [user,setUser]=useState('')
+    function join(e){
+        e.preventDefault()
+        if(user.length>=3){
+          exportuser=user
+          navigate('/chat')
+        }
+        else{
+          alert('Enter valid name')
+        }
+    }
   return (
     <>
         <form action="" >
             <div className='head'></div>
             <div>
-            <input className='component' type="text"  placeholder='Enter Your Name'/>
+            <input className='component' type="text" onChange={(e)=>{setUser(e.target.value)}} value={user} placeholder='Enter Your Name'/>
             </div>
             <div>
-            <input className='component' type="submit" value='Join Chat'/>
+            <input className='component' type="submit" onClick={join} value='Join Chat'/>
             </div>
         </form>
     </>
@@ -16,4 +33,6 @@ function Home() {
 }
 
 export default Home
+
+export {exportuser}
 
