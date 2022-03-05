d233c4936d7d175013f5e6608197151ad076083b
Ankita Kumari <akankitakumari009@gmail.com>
2022-03-05 22:41:20 +0530

    Basic Structure of Home page

---
+++ front_end/src/components/Home.js
@@ -0,0 +1,19 @@
+function Home() {
+
+  return (
+    <>
+        <form action="" >
+            <div className='head'></div>
+            <div>
+            <input className='component' type="text"  placeholder='Enter Your Name'/>
+            </div>
+            <div>
+            <input className='component' type="submit" value='Join Chat'/>
+            </div>
+        </form>
+    </>
+  )
+}
+
+export default Home
+
